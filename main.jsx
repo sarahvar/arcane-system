f8f707813d0a45b1b89d4746b6d9235d37d4681e
sarahvar <sarahvarin95@gmail.com>
2026-02-02 01:58:35 +0100

    index.html

--- main.jsx
+++ main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Root from "./app/root.jsx";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Root />
