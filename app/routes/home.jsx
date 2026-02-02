c9e471ddb41962c0be2461c9987bd5a457def2ba
sarahvar <sarahvarin95@gmail.com>
2026-02-02 01:13:13 +0100

    Welcome

--- app/routes/home.jsx
+++ app/routes/home.jsx
@@ -1,11 +1,4 @@
-import { Welcome } from "../welcome/welcome";
-
-export function meta() {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+import { Welcome } from "../welcome/welcome.jsx";
 
 export default function Home() {
   return <Welcome />;
