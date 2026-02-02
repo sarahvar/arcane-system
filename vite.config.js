d1f5343493ba1890242040c29d71c299081a020b
sarahvar <sarahvarin95@gmail.com>
2026-02-02 01:31:04 +0100

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: ".", // ← assure que la racine est bien le dossier actuel
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: "index.html", // ← point d’entrée explicite
-    },
-  },
 });
