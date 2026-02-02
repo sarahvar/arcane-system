d8ec4b7ca3b26a9141406ce745693da927db7c2b
sarahvar <sarahvarin95@gmail.com>
2026-02-02 01:25:23 +0100

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  root: ".", // ← assure que la racine est bien le dossier actuel
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "index.html", // ← point d’entrée explicite
+    },
+  },
 });
